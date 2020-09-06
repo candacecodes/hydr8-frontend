ce5554226de890fb69bbecfc95619f84850198a1
candacecodes <candacehchoe@gmail.com>
2020-09-06 12:24:44 -0700

    added profile content

---
+++ script.js
@@ -0,0 +1,12 @@
+let profile = document.getElementById('profile')
+let info = document.createElement('div')
+info.innerHTML = 
+`   <center>
+    Name: <br>
+    Age: <br>
+    Gender: <br>
+    Activity Level: <br>
+    Water Goal: <br>
+    </center>
+`
+profile.appendChild(info)

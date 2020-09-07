1512da41ac678d1944450d75042fb1f5b38b5b58
candacecodes <candacehchoe@gmail.com>
2020-09-07 12:01:34 -0700

    added login js code

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
-let profile = document.getElementById('profile')
-let info = document.createElement('div')
-info.innerHTML = 
-`   <center>
-    Name: <br>
-    Age: <br>
-    Gender: <br>
-    Water Goal: <br>
-    </center>
-`
-profile.appendChild(info)
+document.addEventListener('DOMContentLoaded', (e) => {
+
+    let success = false 
+    while (!success) {
+        success = login() 
+        return success
+    }
+}) 
+
+let user 
+
+const login = () => { 
+    let return_value = false 
+    let loginForm = document.getElementById('login-form') 
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault() 
+
+        fetch('http://localhost:3000/users') 
+        .then(res => res.json()) 
+        .then(json => {
+            let foundUser
+
+            json.forEach(user => {
+                if (user.name === e.target[0].value) {
+                    let landingDiv = document.getElementById('profile') 
+                    foundUser = user 
+                    return_value = true
+                    landingDiv.innerText = `Login successful, welcome ${user.name}.`
+
+                    renderWaterCups(foundUser) // finds matching waterCups with userID
+                    
+                }
+            })
+            if (!foundUser) {
+                let error = document.getElementById('login-error')
+                error.innerText = "" 
+                error.innerText = "Sorry, name is not found. Please try again or sign up."
+                loginForm.reset()
+            }
+        })
+
+    })
+} 
+
+const renderWaterCups = (e) => {
+    console.log(e)
+     let profile = document.getElementById('profile')
+     let info = document.createElement('div')
+     info.innerHTML = `
+            <center>
+             Name: ${e.name}<br>
+             Age: ${e.age}<br>
+             Gender: ${e.gender}<br>
+             Water Goal: ${e.watergoal}<br>
+            </center>
+         `
+     profile.appendChild(info)
+} 
